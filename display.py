c95b9d84d7c97310ea0edfabbe4aef8a1ba49ed6
Chris Niznik <chrisphilipniznik@gmail.com>
2019-07-23 12:52:05 +0200

    updated_display

--- display.py
+++ display.py
@@ -1,4 +1,22 @@
-def print_albums_list(album):
-     for album in album:
-         print(' | '.join(album))
+def print_single_album(album):
+        print(' | '.join(album))
+
+def print_multiple_albums(albums):
+        for album in albums:
+                print(' | '.join(album))
+
+def print_start_menu(albums):
+        print("Music database")
+        print("Currently, there are ", len(albums))
+        print("* - display all albums")
+        print("g - display albums by genre")
+        print("t - display albums from giving time range")
+        print("s - display shortest album")
+        print("s - display longest album")
+        print("a - display albums by artist")
+        print("n - display albums by name")
+        print("v - view playlist")
+
+def print_add_playlist():
+        print("Would you like to add this track to playlist?")
 
